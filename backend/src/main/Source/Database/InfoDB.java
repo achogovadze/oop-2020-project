6bebb8eb7401ceccdac79ce8980ec123737c8e9b
Hercules270 <ibezh17@freeuni.edu.ge>
2020-07-26 21:52:47 +0400

    InfoDB added

---
+++ backend/src/main/Source/Database/InfoDB.java
@@ -0,0 +1,8 @@
+package Database;
+
+public class InfoDB {
+    public static final String MYSQL_USERNAME = "root";
+    public static final String MYSQL_PASSWORD = "BlackJack";
+    public static final String MYSQL_DATABASE_SERVER = "localhost";
+    public static final String MYSQL_DATABASE_NAME = "oop_admin";
+}
