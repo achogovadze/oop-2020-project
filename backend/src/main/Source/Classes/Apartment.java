cf9172a0e9b5f6d3255ea73353265eeabac628e7
Taso <anikolaishvili@affix.pro>
2020-07-26 21:12:22 +0400

    create Apartment class

---
+++ backend/src/main/Source/Classes/Apartment.java
@@ -0,0 +1,41 @@
+package Classes;
+
+import java.util.Objects;
+
+public class Apartment {
+
+    private Integer apartmentId;
+    private Integer userId;
+
+    public Apartment(Integer aptId, Integer userId) {
+        this.apartmentId = aptId;
+        this.userId = userId;
+    }
+
+
+    public Integer getApartmentId() {
+        return apartmentId;
+    }
+
+    public void setApartmentId(Integer apartmentId) {
+        this.apartmentId = apartmentId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apartment apartment = (Apartment) o;
+        return Objects.equals(apartmentId, apartment.apartmentId) &&
+                Objects.equals(userId, apartment.userId);
+    }
+
+}
