ae860d923f64b6f219c41ed9dfd92b434835bff5
taso-n <aniko17@freeuni.edu.ge>
2020-07-28 21:38:02 +0400

    create booking DB

---
+++ backend/src/main/Source/Database/BookingsDB.java
@@ -0,0 +1,75 @@
+package Database;
+
+import Classes.Booking;
+import Classes.User;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BookingsDB {
+    static String account = InfoDB.MYSQL_USERNAME;
+    static String password = InfoDB.MYSQL_PASSWORD;
+    static String server = InfoDB.MYSQL_DATABASE_SERVER;
+    static String database = InfoDB.MYSQL_DATABASE_NAME;
+
+    private static BookingsDB db;
+    private Connection connection;
+
+    public BookingsDB() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://"
+                + server, account, password);
+        Statement statement = connection.createStatement();
+        statement.executeQuery("USE " + database);
+    }
+
+    public void commit() throws SQLException {
+        Statement stm = connection.createStatement();
+        stm.executeUpdate("commit;");
+    }
+
+    public static BookingsDB getInstance() throws SQLException, ClassNotFoundException {
+        if (db == null) {
+            synchronized (ApartmentsDB.class) {
+                if (db == null) {
+                    db = new BookingsDB();
+                }
+            }
+        }
+        return db;
+    }
+
+    public void book (Date checkInDate, Date checkOutDate, Integer userId, Integer apartmentId, Double totalPrice, String paymentCurrency) throws SQLException {
+        Integer bookingId = -1;
+        String query = "INSERT INTO bookings (apartment_id, user_id, check_in_date, check_out_date, total_price, payment_currency) VALUES (?, ?, ?, ?, ?, ?);";
+        PreparedStatement stm = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        stm.setInt(1, apartmentId);
+        stm.setInt(2, userId);
+        stm.setDate(3, (java.sql.Date) checkInDate);
+        stm.setDate(4, (java.sql.Date) checkOutDate);
+        stm.setDouble(5, totalPrice);
+        stm.setString(6, paymentCurrency);
+    }
+
+    public List<Booking> getBookings(Integer apartmentId) throws SQLException {
+        List<Booking> result = new ArrayList<>();
+        Statement stm = connection.createStatement();
+        ResultSet res = stm.executeQuery("SELECT * FROM bookings WHERE apartment_id= " + apartmentId);
+
+        while (res.next()) {
+            Booking booking = new Booking(
+                    res.getInt(1),
+                    res.getDate(2),
+                    res.getDate(3),
+                    res.getInt(4),
+                    res.getDouble(5),
+                    res.getString(6),
+                    res.getInt(7));
+            result.add(booking);
+        }
+        return result;
+    }
+}
