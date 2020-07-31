aa352f93da71082844c19fee07ed55f5c8672217
Hercules270 <ibezh17@freeuni.edu.ge>
2020-08-01 00:41:48 +0400

    Bug fix in ApartmentsDB

--- backend/src/main/Source/Database/ApartmentsDB.java
+++ backend/src/main/Source/Database/ApartmentsDB.java
@@ -86,26 +86,13 @@ public class ApartmentsDB {
         return true;
     }
 
-    public boolean deleteApartmentByUser(Integer userId) {
-        try {
-            PreparedStatement stm;
-            stm = connection.prepareStatement("DELETE FROM apartments WHERE user_id = " + userId);
-            stm.executeUpdate();
-            commit();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
     public boolean addApartmentInfo(Integer apartmentId, String country, String city, String address,
                                     Double price, String propertyType, String projectType, String apartmentStatus,
                                     String additionalSpace, Double additionalFootage, Double squareFootage, String buildingMaterials,
                                     Double roomHeight, Integer floorLevel, Integer bedroomsNum, Integer bedsNum,
                                     Integer bathroomsNum, Integer privateBathroom, Integer wifi, String water,
                                     String gas, String electricity, Integer airCondition, String heating,
-                                    String washingMachine, Integer oven, Integer dishwasher, Integer dryer,
+                                    Integer washingMachine, Integer oven, Integer dishwasher, Integer dryer,
                                     Integer desk, Integer balcony, Integer elevator, Integer parking,
                                     Integer vehicleCharger, Integer pool, Integer isAvailable, Double latitude, Double longitude) {
 
@@ -141,7 +128,7 @@ public class ApartmentsDB {
             s.setString(21, electricity);
             s.setInt(22, airCondition);
             s.setString(23, heating);
-            s.setString(24, washingMachine);
+            s.setInt(24, washingMachine);
             s.setInt(25, oven);
             s.setInt(26, dishwasher);
             s.setInt(27, dryer);
@@ -168,19 +155,6 @@ public class ApartmentsDB {
         try {
             PreparedStatement stm;
             stm = connection.prepareStatement("DELETE FROM apartment_info WHERE apartment_id = " + apartmentId);
-            stm.executeUpdate();
-            commit();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    public boolean deleteApartmentInfoByUser(Integer userId) {
-        try {
-            PreparedStatement stm;
-            stm = connection.prepareStatement("DELETE FROM apartment_info where apartment_id in (select apartment_id from apartments where user_id = " + userId + ")");
             stm.executeUpdate();
             commit();
         } catch (SQLException e) {
@@ -239,7 +213,7 @@ public class ApartmentsDB {
             toAdd.add("address = \"" + address + "\"");
         }
         if (!apartmentInfo.getPrice().equals(price)) {
-            toAdd.add("price = " + price);
+            toAdd.add("price_usd = " + price);
         }
         if (!apartmentInfo.getPropertyType().equals(propertyType)) {
             toAdd.add("property_type = \"" + propertyType + "\"");
@@ -290,7 +264,7 @@ public class ApartmentsDB {
             toAdd.add("electricity = \"" + electricity + "\"");
         }
         if (!apartmentInfo.getAirCondition().equals(airCondition)) {
-            toAdd.add("air_condition = " + airCondition);
+            toAdd.add("air_conditioning = " + airCondition);
         }
         if (!apartmentInfo.getHeating().equals(heating)) {
             toAdd.add("heating = \"" + heating + "\"");
@@ -365,44 +339,44 @@ public class ApartmentsDB {
         }
         if (s.next()) {
             ApartmentInfo apartmentInfo = new ApartmentInfo(
-                    s.getInt(1),
-                    s.getString(2),
-                    s.getString(3),
-                    s.getString(4),
-                    s.getDouble(5),
-                    s.getString(6),
-                    s.getString(7),
-                    s.getString(8),
-                    s.getDouble(9),
-                    s.getDouble(10),
-                    s.getString(11),
-                    s.getDouble(12),
-                    s.getInt(13),
-                    s.getInt(14),
-                    s.getInt(15),
-                    s.getInt(16),
-                    s.getInt(17),
-                    s.getInt(18),
-                    s.getString(19),
-                    s.getString(20),
-                    s.getString(21),
-                    s.getInt(22),
-                    s.getString(23),
-                    s.getInt(24),
-                    s.getInt(25),
-                    s.getInt(26),
-                    s.getInt(27),
-                    s.getInt(28),
-                    s.getInt(29),
-                    s.getInt(30),
-                    s.getInt(31),
-                    s.getInt(32),
-                    s.getInt(33),
-                    s.getInt(34),
-                    s.getString(35),
-                    s.getDouble(36),
-                    s.getDouble(37),
-                    imagesList);
+            s.getInt(1),
+            s.getString(2),
+            s.getString(3),
+            s.getString(4),
+            s.getDouble(5),
+            s.getString(6),
+            s.getString(7),
+            s.getString(8),
+            s.getDouble(9),
+            s.getDouble(10),
+            s.getString(11),
+            s.getDouble(12),
+            s.getInt(13),
+            s.getInt(14),
+            s.getInt(15),
+            s.getInt(16),
+            s.getInt(17),
+            s.getInt(18),
+            s.getString(19),
+            s.getString(20),
+            s.getString(21),
+            s.getInt(22),
+            s.getString(23),
+            s.getInt(24),
+            s.getInt(25),
+            s.getInt(26),
+            s.getInt(27),
+            s.getInt(28),
+            s.getInt(29),
+            s.getInt(30),
+            s.getInt(31),
+            s.getInt(32),
+            s.getInt(33),
+            s.getInt(34),
+            s.getString(35),
+            s.getDouble(36),
+            s.getDouble(37),
+            imagesList);
             return apartmentInfo;
         }
         return null;
@@ -437,22 +411,5 @@ public class ApartmentsDB {
             e.printStackTrace();
         }
         return apartmentIDs;
-    }
-
-    public boolean addImages(Integer apartmentId, String link) {
-        String query = "INSERT INTO images (apartment_id, image_link) " +
-                "VALUES (?, ?);";
-        PreparedStatement s;
-        try {
-            s = connection.prepareStatement(query);
-            s.setInt(1, apartmentId);
-            s.setString(2, link);
-            s.executeUpdate();
-            commit();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
     }
 }
